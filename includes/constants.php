7be736d3c16442fbf7288b3310f17a536cc702d8
Arup Sen <arup@arupdesigns.co.uk>
2014-06-02 13:48:05 +0100

    removed comments

--- includes/constants.php
+++ includes/constants.php
@@ -1,10 +1,10 @@
 <?php
 // a few file paths and whatever else doesn't change
-// // -- constants.php --
-// define('IMGPATH', './images/');
-// define('DATAPATH', './data/');
-// define('CSSPATH', './css/');
-// define('JSPATH', './js/');
-//
-//
-// ?>
+// -- constants.php --
+define('IMGPATH', './images/');
+define('DATAPATH', './data/');
+define('CSSPATH', './css/');
+define('JSPATH', './js/');
+
+
+?>
