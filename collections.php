ed3506234f73a229f35042e8c1f229a096232384
Arup Sen <arupsen48@gmail.com>
2014-06-30 01:21:21 +0100

    set up img tags and figure elements for slides

--- collections.php
+++ collections.php
@@ -6,6 +6,27 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
+<div id="NAV-ID" class="crsl-nav">
+  <a href="#" class="previous">Previous</a>
+  <a href="#" class="next">Next</a>
+</div>
+<div class="crsl-items" data-navigation="NAV-ID">
+  <div class="crsl-wrap">
+<?php
+$collection_images = array(
+  'ring1','ring2','ring3',
+  'cuff1','cuff2','cuff3',
+  'earrings'
+);
+$count = count($collection_images);
+for ($i = 0; $i < $count; $i++) {
+  echo '<figure class="crsl-item">';
+  img_tag($collection_images[$i].'.jpg','','');
+  echo '</figure>';
+}
+?>
+  </div>
+</div>
 
 
 <?php 
