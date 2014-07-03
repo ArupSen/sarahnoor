6c2cb2702054422fe07531820cc824f802ab8c5f
Arup Sen <arupsen48@gmail.com>
2014-07-03 21:53:37 +0100

    added texts for each ring / piece

--- collections.php
+++ collections.php
@@ -19,15 +19,40 @@ $collection_images = array(
   'earrings'
 );
 $collection_text = array(
+  'Multi Row Ring<br />
+  Diamond wt.ct: .78ct<br />
+  Metal wt. g: 1.994 <br />
+  Composed of 18k yellow gold',
+  'Baby BagueLe Ring<br />
+  Diamond wt.ct: .43ct<br />
+  Metal wt. g: 1.234 <br />
+  Composed of 18k yellow gold',
+  'Swirl Ring<br />
+  Diamond wt.ct: .40ct<br />
+  Metal wt. g: 0.938 <br />
+  Composed of 18k yellow gold',
+  'Multi Row Ear Cuff<br />
+  Diamond wt.ct: .38ct<br />
+  Metal wt. g: 1.378 <br />
+  Composed of 18k yellow gold',
+  'Triple Hoop Ear Cuff <br />
+  Diamond wt.ct: .30ct<br />
+  Metal wt. g: 1.648 <br />
+  Composed of 18k yellow gold',
   'Single Ear Cuff<br />
   Diamond wt.ct: .15ct<br />
-  Metal wt. g: 0.856<br /> Composed of 18k yellow gold'
+  Metal wt. g: 0.856<br /> 
+  Composed of 18k yellow gold',
+  'Baby BagueLe Earring<br />
+  Diamond wt.ct: .55ct<br />
+  Metal wt. g: 1.278 <br />
+  Composed of 18k yellow gold'
 );
 $count = count($collection_images);
 for ($i = 0; $i < $count; $i++) {
   echo '<figure class="crsl-item">';
   img_tag($collection_images[$i].'.jpg','','');
-  echo '<p>'.$collection_text[0].'</p>';
+  echo '<p>'.$collection_text[$i].'</p>';
   echo '</figure>';
 }
 ?>
