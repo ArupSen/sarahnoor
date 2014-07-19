e538e0d09c8e8ecc93e131974736595bad0a6f7b
Arup Sen <arupsen48@gmail.com>
2014-07-19 14:20:27 +0100

    created an included snippet for repeating code in collection pages

---
+++ includes/create_slides.php
@@ -0,0 +1,22 @@
+<?php
+// -- create_slides.php --
+// set the slide navigation and create images with captions
+// for the collection pages
+?>
+<div id="NAV-ID" class="crsl-nav">
+  <a href="#" class="previous">&lt;</a>
+  <a href="#" class="next">&gt;</a>
+</div>
+<div class="crsl-items" data-navigation="NAV-ID">
+  <div class="crsl-wrap">
+<?php
+$count = count($collection_images);
+for ($i = 0; $i < $count; $i++) {
+  echo '<figure class="crsl-item">';
+  img_tag($collection_images[$i][0].'.jpg','','');
+  echo '<p>'.$collection_images[$i][1].'</p>';
+  echo '</figure>';
+}
+?>
+  </div><!-- end of .crsl-wrap -->
+</div><!-- end of .crsl-items -->
