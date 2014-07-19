04d49a458608e8f9fdaa9624d9415e27b296adf5
Arup Sen <arupsen48@gmail.com>
2014-07-19 14:21:42 +0100

    merged image and text arrays, removed repeating code to snippet

--- collections.php
+++ collections.php
@@ -5,63 +5,36 @@ require_once INCPATH.'header.php';
 navbar();
 
 // the unique contents will go here up to the footer
-?>
-<div id="NAV-ID" class="crsl-nav">
-  <a href="#" class="previous">&lt;</a>
-  <a href="#" class="next">&gt;</a>
-</div>
-<div class="crsl-items" data-navigation="NAV-ID">
-  <div class="crsl-wrap">
-<?php
 $collection_images = array(
-  'ring1','ring2','ring3',
-  'cuff1','cuff2','cuff3',
-  'earrings'
-);
-$collection_text = array(
-  'Multi Row Ring<br />
+  array('ring1','Multi Row Ring<br />
   Diamond wt.ct: .78ct<br />
   Metal wt. g: 1.994 <br />
-  Composed of 18k yellow gold',
-  'Baby BagueLe Ring<br />
+  Composed of 18k yellow gold'),
+  array('ring2','Baby BagueLe Ring<br />
   Diamond wt.ct: .43ct<br />
   Metal wt. g: 1.234 <br />
-  Composed of 18k yellow gold',
-  'Swirl Ring<br />
+  Composed of 18k yellow gold'),
+  array('ring3','Swirl Ring<br />
   Diamond wt.ct: .40ct<br />
   Metal wt. g: 0.938 <br />
-  Composed of 18k yellow gold',
-  'Multi Row Ear Cuff<br />
+  Composed of 18k yellow gold'),
+  array('cuff1','Multi Row Ear Cuff<br />
   Diamond wt.ct: .38ct<br />
   Metal wt. g: 1.378 <br />
-  Composed of 18k yellow gold',
-  'Triple Hoop Ear Cuff <br />
+  Composed of 18k yellow gold'),
+  array('cuff2','Triple Hoop Ear Cuff <br />
   Diamond wt.ct: .30ct<br />
   Metal wt. g: 1.648 <br />
-  Composed of 18k yellow gold',
-  'Single Ear Cuff<br />
+  Composed of 18k yellow gold'),
+  array('cuff3','Single Ear Cuff<br />
   Diamond wt.ct: .15ct<br />
   Metal wt. g: 0.856<br /> 
-  Composed of 18k yellow gold',
-  'Baby BagueLe Earring<br />
+  Composed of 18k yellow gold'),
+  array('earrings','Baby BagueLe Earring<br />
   Diamond wt.ct: .55ct<br />
   Metal wt. g: 1.278 <br />
-  Composed of 18k yellow gold'
+  Composed of 18k yellow gold')
 );
-$count = count($collection_images);
-for ($i = 0; $i < $count; $i++) {
-  echo '<figure class="crsl-item">';
-  img_tag($collection_images[$i].'.jpg','','');
-  echo '<p>'.$collection_text[$i].'</p>';
-  echo '</figure>';
-}
+require_once INCPATH.'create_slides.php';
 ?>
-  </div>
-</div>
-
-
-<?php 
-// filename, title, id
-// img_tag('','',''); ?>
-
 <?php footer(); ?>
--- pyramids.php
+++ pyramids.php
@@ -5,62 +5,32 @@ require_once INCPATH.'header.php';
 navbar();
 
 // the unique contents will go here up to the footer
-?>
-<div id="NAV-ID" class="crsl-nav">
-  <a href="#" class="previous">&lt;</a>
-  <a href="#" class="next">&gt;</a>
-</div>
-<div class="crsl-items" data-navigation="NAV-ID">
-  <div class="crsl-wrap">
-<?php
 $collection_images = array(
-  'bangle','Bracelet1','Earrings1',
-  'Earrings2','Necklace1','Necklace2'
-);
-$collection_text = array(
-  'Pyramid Bangle<br />
+  array('bangle','Pyramid Bangle<br />
 Diamond wt.ct: .27ct<br />
 Metal wt. g: 8.810 <br />
-Composed of 18k yellow gold',
-  'Pyamid Bracelet Alternating Diamonds (Large)<br />
+Composed of 18k yellow gold'),
+  array('Bracelet1','Pyamid Bracelet Alternating Diamonds (Large)<br />
 Diamond wt.ct: 3.5ct<br />
 Metal wt.g: 28<br />
-Composed of 18k yellow gold',
-  'Swirl Ring<br />
+Composed of 18k yellow gold'),
+  array('Earrings1','Swirl Ring<br />
   Diamond wt.ct: .40ct<br />
   Metal wt. g: 0.938 <br />
-  Composed of 18k yellow gold',
-  'Multi Row Ear Cuff<br />
+  Composed of 18k yellow gold'),
+  array('Earrings2','Multi Row Ear Cuff<br />
   Diamond wt.ct: .38ct<br />
   Metal wt. g: 1.378 <br />
-  Composed of 18k yellow gold',
-  'Triple Hoop Ear Cuff <br />
+  Composed of 18k yellow gold'),
+  array('Necklace1','Triple Hoop Ear Cuff <br />
   Diamond wt.ct: .30ct<br />
   Metal wt. g: 1.648 <br />
-  Composed of 18k yellow gold',
-  'Single Ear Cuff<br />
+  Composed of 18k yellow gold'),
+  array('Necklace2','Single Ear Cuff<br />
   Diamond wt.ct: .15ct<br />
   Metal wt. g: 0.856<br /> 
-  Composed of 18k yellow gold',
-  'Baby BagueLe Earring<br />
-  Diamond wt.ct: .55ct<br />
-  Metal wt. g: 1.278 <br />
-  Composed of 18k yellow gold'
+  Composed of 18k yellow gold')
 );
-$count = count($collection_images);
-for ($i = 0; $i < $count; $i++) {
-  echo '<figure class="crsl-item">';
-  img_tag($collection_images[$i].'.jpg','','');
-  echo '<p>'.$collection_text[$i].'</p>';
-  echo '</figure>';
-}
+require_once INCPATH.'create_slides.php';
 ?>
-  </div>
-</div>
-
-
-<?php 
-// filename, title, id
-// img_tag('','',''); ?>
-
 <?php footer(); ?>
