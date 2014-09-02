301d11fefa7972dbf3f6ff1fb64d2607a0afb9b7
Arup Sen <arupsen48@gmail.com>
2014-09-02 20:23:22 +0100

    centred content with bootstrap columns and added content

--- contact.php
+++ contact.php
@@ -6,10 +6,21 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-
-
-<?php 
-// filename, title, id
-// img_tag('','',''); ?>
-
+<div class="row">
+<div class="col-md-3"></div>
+<div class="col-md-4">
+<?php img_tag('contact-page.jpg','','contact-pic'); ?>
+</div><!-- .col-md-4 -->
+  <div class="col-md-2">
+<div class="numbers">
+<dl>
+    <dt>General Inquiries</dt>
+<dd>Info@sarahnoor.com</dd><dd> 02071938858</dd>
+    <dt>Orders and wholesale</dt> <dd>orders@sarahnoor.com</dd>
+    <dt>press</dt> <dd>sophie@5thhousepr.com</dd> <dd>02075808519</dd>
+</dl>
+</div><!-- .col-md-2 -->
+<div class="col-md-3"></div>
+</div>
+</div>
 <?php footer(); ?>
