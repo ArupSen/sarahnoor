64b14a693450a4d2f752907aa2ec7174fd2de5d9
Arup Sen <arupsen48@gmail.com>
2014-09-10 23:51:51 +0100

    removed Metal wt. lines from descriptions

--- pretty.php
+++ pretty.php
@@ -1,5 +1,5 @@
 <?php
-// -- colletions.php --
+// -- pretty.php --
 require_once 'inc_path.php';
 require_once INCPATH.'header.php';
 navbar();
@@ -8,31 +8,24 @@ navbar();
 $collection_images = array(
   array('ring1','Multi Row Ring<br />
   Diamond wt.ct: .78ct<br />
-  Metal wt. g: 1.994 <br />
   Composed of 18k yellow gold'),
   array('ring2','Baby BagueLe Ring<br />
   Diamond wt.ct: .43ct<br />
-  Metal wt. g: 1.234 <br />
   Composed of 18k yellow gold'),
   array('ring3','Swirl Ring<br />
   Diamond wt.ct: .40ct<br />
-  Metal wt. g: 0.938 <br />
   Composed of 18k yellow gold'),
   array('cuff1','Multi Row Ear Cuff<br />
   Diamond wt.ct: .38ct<br />
-  Metal wt. g: 1.378 <br />
   Composed of 18k yellow gold'),
   array('cuff2','Triple Hoop Ear Cuff <br />
   Diamond wt.ct: .30ct<br />
-  Metal wt. g: 1.648 <br />
   Composed of 18k yellow gold'),
   array('cuff3','Single Ear Cuff<br />
   Diamond wt.ct: .15ct<br />
-  Metal wt. g: 0.856<br /> 
   Composed of 18k yellow gold'),
   array('earrings','Baby BagueLe Earring<br />
   Diamond wt.ct: .55ct<br />
-  Metal wt. g: 1.278 <br />
   Composed of 18k yellow gold')
 );
 require_once INCPATH.'create_slides.php';
--- pyramids.php
+++ pyramids.php
@@ -8,27 +8,21 @@ navbar();
 $collection_images = array(
   array('bangle','Pyramid Bangle<br />
 Diamond wt.ct: .27ct<br />
-Metal wt. g: 8.810 <br />
 Composed of 18k yellow gold'),
   array('Bracelet1','Pyamid Bracelet Alternating Diamonds (Large)<br />
 Diamond wt.ct: 3.5ct<br />
-Metal wt.g: 28<br />
 Composed of 18k yellow gold'),
   array('Earrings1','Swirl Ring<br />
   Diamond wt.ct: .40ct<br />
-  Metal wt. g: 0.938 <br />
   Composed of 18k yellow gold'),
   array('Earrings2','Multi Row Ear Cuff<br />
   Diamond wt.ct: .38ct<br />
-  Metal wt. g: 1.378 <br />
   Composed of 18k yellow gold'),
   array('Necklace1','Triple Hoop Ear Cuff <br />
   Diamond wt.ct: .30ct<br />
-  Metal wt. g: 1.648 <br />
   Composed of 18k yellow gold'),
   array('Necklace2','Single Ear Cuff<br />
   Diamond wt.ct: .15ct<br />
-  Metal wt. g: 0.856<br /> 
   Composed of 18k yellow gold')
 );
 require_once INCPATH.'create_slides.php';
