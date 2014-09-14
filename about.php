72c10080e9a24d8f1f56b383512c538f1d58e48b
Arup Sen <arupsen48@gmail.com>
2014-09-14 13:53:33 +0100

    created about and campaign pages

--- about.php
+++ about.php
@@ -6,10 +6,24 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
+<div class="col-md-3">
+<?php
+img_tag('about-birds.jpg','');
+?>
+</div>
+<div class="col-md-6">
+<p>Sarah Noor Jewellery is the creation of London based fine jewellery designer Sarah Al-Hassan. The onymous fine jewellery line embodies timeless designs with modern aesthetics. </p>
 
-
-<?php 
-// filename, title, id
-// img_tag('','',''); ?>
+<p>With a Focus on understated elegance, Sarah was inspired to create her first collection of classic staples with a modern twist. From her dainty rings and ear cuffs to her bold pyramid bracelet, Sarah Noor jewellery is the perfect addition to every woman’s jewellery box.</p>
+ 
+<p>Sarah Noor Jewellery is commited to trendy yet timeless handcrafted pieces. Every piece is skillfully handmade to perfection by some of the world’s most skilled artisans in London. Her team includes casters and setters who have worked for the world’s most prestigious jewellery houses.</p>
+ 
+<p>Sarah Noor Jewellery redefines traditional staples by using only precious materials that include the highest grade of conflict free diamonds, and 18k gold. Quality is paramount in every step of creating a Sarah Noor piece, unraveling only the finest and luxurious jewellery for every woman.</p>
+</div>
+<div class="col-md-3">
+<?php
+img_tag('about-sarah.jpg','');
+?>
+</div>
 
 <?php footer(); ?>
--- campaign.php
+++ campaign.php
@@ -5,11 +5,27 @@ require_once INCPATH.'header.php';
 navbar();
 
 // the unique contents will go here up to the footer
+$collection_images = array(
+  array('campaign1','Multi Row Ring<br />
+  Diamond wt.ct: .78ct<br />
+  Composed of 18k yellow gold'),
+  array('campaign2','Baby BagueLe Ring<br />
+  Diamond wt.ct: .43ct<br />
+  Composed of 18k yellow gold'),
+  array('campaign3','Swirl Ring<br />
+  Diamond wt.ct: .40ct<br />
+  Composed of 18k yellow gold'),
+  array('campaign4','Multi Row Ear Cuff<br />
+  Diamond wt.ct: .38ct<br />
+  Composed of 18k yellow gold'),
+  array('campaign5','Triple Hoop Ear Cuff <br />
+  Diamond wt.ct: .30ct<br />
+  Composed of 18k yellow gold'),
+  array('campaign6','Single Ear Cuff<br />
+  Diamond wt.ct: .15ct<br />
+  Composed of 18k yellow gold')
+);
+require_once INCPATH.'create_slides.php';
 ?>
-
-
-<?php 
-// filename, title, id
-// img_tag('','',''); ?>
 
 <?php footer(); ?>
