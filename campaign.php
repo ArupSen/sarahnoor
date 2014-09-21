aa0d720f97306173afe3a77b6b8509aad6033464
Arup Sen <arupsen48@gmail.com>
2014-09-21 01:31:36 +0100

    removed all text

--- campaign.php
+++ campaign.php
@@ -1,29 +1,17 @@
 <?php
-// -- press.php --
+// -- campaign.php --
 require_once 'inc_path.php';
 require_once INCPATH.'header.php';
 navbar();
 
 // the unique contents will go here up to the footer
 $collection_images = array(
-  array('campaign1','Multi Row Ring<br />
-  Diamond wt.ct: .78ct<br />
-  Composed of 18k yellow gold'),
-  array('campaign2','Baby BagueLe Ring<br />
-  Diamond wt.ct: .43ct<br />
-  Composed of 18k yellow gold'),
-  array('campaign3','Swirl Ring<br />
-  Diamond wt.ct: .40ct<br />
-  Composed of 18k yellow gold'),
-  array('campaign4','Multi Row Ear Cuff<br />
-  Diamond wt.ct: .38ct<br />
-  Composed of 18k yellow gold'),
-  array('campaign5','Triple Hoop Ear Cuff <br />
-  Diamond wt.ct: .30ct<br />
-  Composed of 18k yellow gold'),
-  array('campaign6','Single Ear Cuff<br />
-  Diamond wt.ct: .15ct<br />
-  Composed of 18k yellow gold')
+  array('campaign1',''),
+  array('campaign2',''),
+  array('campaign3',''),
+  array('campaign4',''),
+  array('campaign5',''),
+  array('campaign6','')
 );
 require_once INCPATH.'create_slides.php';
 ?>
