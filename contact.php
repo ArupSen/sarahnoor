f49191391459c9bc147ca5e82f55cde431d014b8
Arup Sen <arupsen48@gmail.com>
2014-09-21 01:32:14 +0100

    added text and made image div bigger

--- contact.php
+++ contact.php
@@ -7,17 +7,19 @@ navbar();
 // the unique contents will go here up to the footer
 ?>
 <div class="row">
-<div class="col-md-3"></div>
-<div class="col-md-4">
+<div class="col-md-2"></div>
+<div class="col-md-5">
 <?php img_tag('contact-page.jpg','','contact-pic'); ?>
 </div><!-- .col-md-4 -->
   <div class="col-md-2">
 <div class="numbers">
 <dl>
     <dt>General Inquiries</dt>
-<dd>Info@sarahnoor.com</dd><dd> 02071938858</dd>
-    <dt>Orders and wholesale</dt> <dd>orders@sarahnoor.com</dd>
-    <dt>press</dt> <dd>sophie@5thhousepr.com</dd> <dd>02075808519</dd>
+<dd>Info<span>@</span>sarahnoor.com</dd><dd> 02071938858</dd>
+    <dt>Orders and wholesale</dt> <dd>orders<span>@</span>sarahnoor.com</dd>
+    <dt>press</dt> <dd>sophie<span>@</span>5thhousepr.com</dd> <dd>02075808519</dd>
+    <dt>Illustration by</dt> <dd>Florian Meacci</dd> 
+    <dt>website design by</dt> <dd>Alyson Mowat</dd> 
 </dl>
 </div><!-- .col-md-2 -->
 <div class="col-md-3"></div>
