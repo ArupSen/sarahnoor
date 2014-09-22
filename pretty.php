c2bf2a7eafc4810010c2eab36476e81064d75ba8
Arup Sen <arupsen48@gmail.com>
2014-09-22 10:20:41 +0100

    refactored by abstracting repeating elements using for loops

--- pretty.php
+++ pretty.php
@@ -9,32 +9,35 @@ $collection_images = array(
   array('ring1','Multi Row Ring<br />
   Diamond wt.ct: .78ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold'),
+'),
   array('ring2','Baby Baguette Ring<br />
   Diamond wt.ct: .43ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold'),
+'),
   array('ring3','Swirl Ring<br />
   Diamond wt.ct: .40ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold'),
+'),
   array('cuff1','Multi Row Ear Cuff<br />
   Diamond wt.ct: .38ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold'),
+'),
   array('cuff2','Triple Hoop Ear Cuff <br />
   Diamond wt.ct: .30ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold'),
+'),
   array('cuff3','Single Ear Cuff<br />
   Diamond wt.ct: .15ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold'),
+'),
   array('earrings-use','Baby Baguette Earring<br />
   Diamond wt.ct: .55ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold')
+')
 );
+for ($i = 0; $i < count($collection_images); $i++) {
+  $collection_images[$i][1] = $collection_images[$i][1].'Available in rose and white gold';
+}
 require_once INCPATH.'create_slides.php';
 ?>
 <?php footer(); ?>
--- pyramids.php
+++ pyramids.php
@@ -9,28 +9,31 @@ $collection_images = array(
   array('bangle','Pyramid Bangle<br />
 Diamond wt.ct: .27ct<br />
 Composed of 18k yellow gold<br />
-Available in rose and white gold'),
+'),
   array('Bracelet1','Pyamid Bracelet Alternating Diamonds (Large)<br />
 Diamond wt.ct: 3.5ct<br />
 Composed of 18k yellow gold<br />
-Available in rose and white gold'),
+'),
   array('Earrings1','Swirl Ring<br />
   Diamond wt.ct: .40ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold'),
+'),
   array('Earrings2','Multi Row Ear Cuff<br />
   Diamond wt.ct: .38ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold'),
+'),
   array('Necklace1','Triple Hoop Ear Cuff <br />
   Diamond wt.ct: .30ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold'),
+'),
   array('Necklace2','Single Ear Cuff<br />
   Diamond wt.ct: .15ct<br />
   Composed of 18k yellow gold<br />
-  Available in rose and white gold')
+')
 );
+for ($i = 0; $i < count($collection_images); $i++) {
+  $collection_images[$i][1] = $collection_images[$i][1].'Available in rose and white gold';
+}
 require_once INCPATH.'create_slides.php';
 ?>
 <?php footer(); ?>
