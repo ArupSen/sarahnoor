7fa39650ffb33a77901eca08fd009c4fde8dfb41
Arup Sen <arupsen48@gmail.com>
2014-09-25 09:52:34 +0100

    cleaned out google analytics code

--- includes/footer.php
+++ includes/footer.php
@@ -7,16 +7,6 @@
   <!-- <a href="http://arupdesigns.co.uk" onclick="window.open('http://arupdesigns.co.uk');return false"><?php // img_tag('ad_icon.png','site by arup designs'); ?></a> -->
 </div>
 </div> <!-- end of wrap -->
-<!-- the Google Analytics tracker -->
-<script type="text/javascript">
-  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-  document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-  </script>
-  <script type="text/javascript">
-  try {
-  var pageTracker = _gat._getTracker(""); // add the tracker ID
-  pageTracker._trackPageview();
-  } catch(err) {}</script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
